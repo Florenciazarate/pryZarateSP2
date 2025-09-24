67a44c35b9179c9960fe3feefcb24458305ed9e0
Florencia zarate <florenciazarate2110@gmail.com>
2025-09-24 12:55:44 -0300

    style colors

--- pryZarateSP2/frmReserva.Designer.cs
+++ pryZarateSP2/frmReserva.Designer.cs
@@ -28,6 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            Guna.UI2.AnimatorNS.Animation animation2 = new Guna.UI2.AnimatorNS.Animation();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmReserva));
             lblDias = new Label();
             lblPersonas = new Label();
@@ -38,28 +40,31 @@
             lblTitular = new Label();
             lblTelefono = new Label();
             txtNombre = new TextBox();
-            txtTelefono = new TextBox();
             btnAceptar = new Button();
-            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            chkEfectivo = new RadioButton();
-            chkTarjeta = new RadioButton();
+            optEfectivo = new RadioButton();
+            optTarjeta = new RadioButton();
             lstTipo = new ComboBox();
             lstPersonas = new ComboBox();
             numDias = new NumericUpDown();
             panel3 = new Panel();
             panel4 = new Panel();
+            txtNumero = new MaskedTextBox();
             chkTelevisor = new CheckBox();
             chkCocina = new CheckBox();
             chkHeladera = new CheckBox();
             lblAdicionales = new Label();
             panel1 = new Panel();
             panel2 = new Panel();
+            guna2Transition1 = new Guna.UI2.WinForms.Guna2Transition();
+            guna2BorderlessForm1 = new Guna.UI2.WinForms.Guna2BorderlessForm(components);
             ((System.ComponentModel.ISupportInitialize)numDias).BeginInit();
+            panel4.SuspendLayout();
             SuspendLayout();
             // 
             // lblDias
             // 
             lblDias.AutoSize = true;
+            guna2Transition1.SetDecoration(lblDias, Guna.UI2.AnimatorNS.DecorationType.None);
             lblDias.Font = new Font("Nirmala UI Semilight", 12F);
             lblDias.Location = new Point(529, 101);
             lblDias.Name = "lblDias";
@@ -70,6 +75,7 @@
             // lblPersonas
             // 
             lblPersonas.AutoSize = true;
+            guna2Transition1.SetDecoration(lblPersonas, Guna.UI2.AnimatorNS.DecorationType.None);
             lblPersonas.Font = new Font("Nirmala UI Semilight", 12F);
             lblPersonas.Location = new Point(283, 100);
             lblPersonas.Name = "lblPersonas";
@@ -80,6 +86,7 @@
             // lblTipo
             // 
             lblTipo.AutoSize = true;
+            guna2Transition1.SetDecoration(lblTipo, Guna.UI2.AnimatorNS.DecorationType.None);
             lblTipo.Font = new Font("Nirmala UI Semilight", 12F);
             lblTipo.Location = new Point(43, 100);
             lblTipo.Name = "lblTipo";
@@ -90,6 +97,7 @@
             // lblTipoCabaña
             // 
             lblTipoCabaña.AutoSize = true;
+            guna2Transition1.SetDecoration(lblTipoCabaña, Guna.UI2.AnimatorNS.DecorationType.None);
             lblTipoCabaña.Font = new Font("Nirmala UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblTipoCabaña.Location = new Point(41, 39);
             lblTipoCabaña.Name = "lblTipoCabaña";
@@ -100,6 +108,7 @@
             // lblFormasPago
             // 
             lblFormasPago.AutoSize = true;
+            guna2Transition1.SetDecoration(lblFormasPago, Guna.UI2.AnimatorNS.DecorationType.None);
             lblFormasPago.Font = new Font("Nirmala UI", 14.25F, FontStyle.Bold);
             lblFormasPago.Location = new Point(256, 194);
             lblFormasPago.Name = "lblFormasPago";
@@ -110,6 +119,7 @@
             // lblNombre
             // 
             lblNombre.AutoSize = true;
+            guna2Transition1.SetDecoration(lblNombre, Guna.UI2.AnimatorNS.DecorationType.None);
             lblNombre.Font = new Font("Nirmala UI Semilight", 12F);
             lblNombre.Location = new Point(43, 366);
             lblNombre.Name = "lblNombre";
@@ -120,6 +130,7 @@
             // lblTitular
             // 
             lblTitular.AutoSize = true;
+            guna2Transition1.SetDecoration(lblTitular, Guna.UI2.AnimatorNS.DecorationType.None);
             lblTitular.Font = new Font("Nirmala UI", 14.25F, FontStyle.Bold);
             lblTitular.Location = new Point(43, 331);
             lblTitular.Name = "lblTitular";
@@ -130,6 +141,7 @@
             // lblTelefono
             // 
             lblTelefono.AutoSize = true;
+            guna2Transition1.SetDecoration(lblTelefono, Guna.UI2.AnimatorNS.DecorationType.None);
             lblTelefono.Font = new Font("Nirmala UI Semilight", 12F);
             lblTelefono.Location = new Point(43, 404);
             lblTelefono.Name = "lblTelefono";
@@ -140,22 +152,16 @@
             // txtNombre
             // 
             txtNombre.BackColor = Color.BurlyWood;
+            guna2Transition1.SetDecoration(txtNombre, Guna.UI2.AnimatorNS.DecorationType.None);
             txtNombre.Location = new Point(127, 366);
             txtNombre.Name = "txtNombre";
             txtNombre.Size = new Size(295, 23);
             txtNombre.TabIndex = 10;
             // 
-            // txtTelefono
-            // 
-            txtTelefono.BackColor = Color.BurlyWood;
-            txtTelefono.Location = new Point(127, 404);
-            txtTelefono.Name = "txtTelefono";
-            txtTelefono.Size = new Size(295, 23);
-            txtTelefono.TabIndex = 11;
-            // 
             // btnAceptar
             // 
-            btnAceptar.BackColor = Color.SandyBrown;
+            btnAceptar.BackColor = Color.Tan;
+            guna2Transition1.SetDecoration(btnAceptar, Guna.UI2.AnimatorNS.DecorationType.None);
             btnAceptar.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
             btnAceptar.Location = new Point(611, 479);
             btnAceptar.Name = "btnAceptar";
@@ -164,33 +170,36 @@
             btnAceptar.Text = "Aceptar";
             btnAceptar.UseVisualStyleBackColor = false;
             // 
-            // chkEfectivo
+            // optEfectivo
             // 
-            chkEfectivo.AutoSize = true;
-            chkEfectivo.Font = new Font("Nirmala UI", 12F);
-            chkEfectivo.Location = new Point(258, 230);
-            chkEfectivo.Name = "chkEfectivo";
-            chkEfectivo.Size = new Size(82, 25);
-            chkEfectivo.TabIndex = 16;
-            chkEfectivo.TabStop = true;
-            chkEfectivo.Text = "Efectivo";
-            chkEfectivo.UseVisualStyleBackColor = true;
+            optEfectivo.AutoSize = true;
+            guna2Transition1.SetDecoration(optEfectivo, Guna.UI2.AnimatorNS.DecorationType.None);
+            optEfectivo.Font = new Font("Nirmala UI", 12F);
+            optEfectivo.Location = new Point(258, 230);
+            optEfectivo.Name = "optEfectivo";
+            optEfectivo.Size = new Size(82, 25);
+            optEfectivo.TabIndex = 16;
+            optEfectivo.TabStop = true;
+            optEfectivo.Text = "Efectivo";
+            optEfectivo.UseVisualStyleBackColor = true;
             // 
-            // chkTarjeta
+            // optTarjeta
             // 
-            chkTarjeta.AutoSize = true;
-            chkTarjeta.Font = new Font("Nirmala UI", 12F);
-            chkTarjeta.Location = new Point(258, 255);
-            chkTarjeta.Name = "chkTarjeta";
-            chkTarjeta.Size = new Size(73, 25);
-            chkTarjeta.TabIndex = 17;
-            chkTarjeta.TabStop = true;
-            chkTarjeta.Text = "Tarjeta";
-            chkTarjeta.UseVisualStyleBackColor = true;
+            optTarjeta.AutoSize = true;
+            guna2Transition1.SetDecoration(optTarjeta, Guna.UI2.AnimatorNS.DecorationType.None);
+            optTarjeta.Font = new Font("Nirmala UI", 12F);
+            optTarjeta.Location = new Point(258, 255);
+            optTarjeta.Name = "optTarjeta";
+            optTarjeta.Size = new Size(73, 25);
+            optTarjeta.TabIndex = 17;
+            optTarjeta.TabStop = true;
+            optTarjeta.Text = "Tarjeta";
+            optTarjeta.UseVisualStyleBackColor = true;
             // 
             // lstTipo
             // 
             lstTipo.BackColor = Color.BurlyWood;
+            guna2Transition1.SetDecoration(lstTipo, Guna.UI2.AnimatorNS.DecorationType.None);
             lstTipo.FormattingEnabled = true;
             lstTipo.Items.AddRange(new object[] { "A", "B" });
             lstTipo.Location = new Point(109, 100);
@@ -201,6 +210,7 @@
             // lstPersonas
             // 
             lstPersonas.BackColor = Color.BurlyWood;
+            guna2Transition1.SetDecoration(lstPersonas, Guna.UI2.AnimatorNS.DecorationType.None);
             lstPersonas.FormattingEnabled = true;
             lstPersonas.Location = new Point(369, 100);
             lstPersonas.Name = "lstPersonas";
@@ -210,6 +220,7 @@
             // numDias
             // 
             numDias.BackColor = Color.BurlyWood;
+            guna2Transition1.SetDecoration(numDias, Guna.UI2.AnimatorNS.DecorationType.None);
             numDias.Location = new Point(575, 100);
             numDias.Name = "numDias";
             numDias.Size = new Size(120, 23);
@@ -217,6 +228,7 @@
             // 
             // panel3
             // 
+            guna2Transition1.SetDecoration(panel3, Guna.UI2.AnimatorNS.DecorationType.None);
             panel3.Location = new Point(32, 57);
             panel3.Name = "panel3";
             panel3.Size = new Size(690, 117);
@@ -224,14 +236,26 @@
             // 
             // panel4
             // 
+            panel4.Controls.Add(txtNumero);
+            guna2Transition1.SetDecoration(panel4, Guna.UI2.AnimatorNS.DecorationType.None);
             panel4.Location = new Point(32, 345);
             panel4.Name = "panel4";
             panel4.Size = new Size(690, 117);
             panel4.TabIndex = 23;
             // 
+            // txtNumero
+            // 
+            txtNumero.BackColor = Color.BurlyWood;
+            guna2Transition1.SetDecoration(txtNumero, Guna.UI2.AnimatorNS.DecorationType.None);
+            txtNumero.Location = new Point(95, 57);
+            txtNumero.Name = "txtNumero";
+            txtNumero.Size = new Size(139, 23);
+            txtNumero.TabIndex = 0;
+            // 
             // chkTelevisor
             // 
             chkTelevisor.AutoSize = true;
+            guna2Transition1.SetDecoration(chkTelevisor, Guna.UI2.AnimatorNS.DecorationType.None);
             chkTelevisor.Font = new Font("Nirmala UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
             chkTelevisor.Location = new Point(47, 286);
             chkTelevisor.Name = "chkTelevisor";
@@ -243,6 +267,7 @@
             // chkCocina
             // 
             chkCocina.AutoSize = true;
+            guna2Transition1.SetDecoration(chkCocina, Guna.UI2.AnimatorNS.DecorationType.None);
             chkCocina.Font = new Font("Nirmala UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
             chkCocina.Location = new Point(47, 224);
             chkCocina.Name = "chkCocina";
@@ -254,6 +279,7 @@
             // chkHeladera
             // 
             chkHeladera.AutoSize = true;
+            guna2Transition1.SetDecoration(chkHeladera, Guna.UI2.AnimatorNS.DecorationType.None);
             chkHeladera.Font = new Font("Nirmala UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
             chkHeladera.Location = new Point(47, 255);
             chkHeladera.Name = "chkHeladera";
@@ -265,6 +291,7 @@
             // lblAdicionales
             // 
             lblAdicionales.AutoSize = true;
+            guna2Transition1.SetDecoration(lblAdicionales, Guna.UI2.AnimatorNS.DecorationType.None);
             lblAdicionales.Font = new Font("Nirmala UI", 14.25F, FontStyle.Bold);
             lblAdicionales.Location = new Point(47, 194);
             lblAdicionales.Name = "lblAdicionales";
@@ -274,6 +301,7 @@
             // 
             // panel1
             // 
+            guna2Transition1.SetDecoration(panel1, Guna.UI2.AnimatorNS.DecorationType.None);
             panel1.Location = new Point(32, 208);
             panel1.Name = "panel1";
             panel1.Size = new Size(200, 120);
@@ -281,15 +309,43 @@
             // 
             // panel2
             // 
+            guna2Transition1.SetDecoration(panel2, Guna.UI2.AnimatorNS.DecorationType.None);
             panel2.Location = new Point(246, 208);
             panel2.Name = "panel2";
             panel2.Size = new Size(200, 120);
             panel2.TabIndex = 1;
             // 
+            // guna2Transition1
+            // 
+            guna2Transition1.Cursor = null;
+            animation2.AnimateOnlyDifferences = true;
+            animation2.BlindCoeff = (PointF)resources.GetObject("animation2.BlindCoeff");
+            animation2.LeafCoeff = 0F;
+            animation2.MaxTime = 1F;
+            animation2.MinTime = 0F;
+            animation2.MosaicCoeff = (PointF)resources.GetObject("animation2.MosaicCoeff");
+            animation2.MosaicShift = (PointF)resources.GetObject("animation2.MosaicShift");
+            animation2.MosaicSize = 0;
+            animation2.Padding = new Padding(0);
+            animation2.RotateCoeff = 0F;
+            animation2.RotateLimit = 0F;
+            animation2.ScaleCoeff = (PointF)resources.GetObject("animation2.ScaleCoeff");
+            animation2.SlideCoeff = (PointF)resources.GetObject("animation2.SlideCoeff");
+            animation2.TimeCoeff = 0F;
+            animation2.TransparencyCoeff = 0F;
+            guna2Transition1.DefaultAnimation = animation2;
+            // 
+            // guna2BorderlessForm1
+            // 
+            guna2BorderlessForm1.ContainerControl = this;
+            guna2BorderlessForm1.DockIndicatorTransparencyValue = 0.6D;
+            guna2BorderlessForm1.TransparentWhileDrag = true;
+            // 
             // frmReserva
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            AutoValidate = AutoValidate.EnablePreventFocusChange;
             BackColor = Color.AntiqueWhite;
             ClientSize = new Size(760, 580);
             Controls.Add(lblAdicionales);
@@ -299,10 +355,9 @@
             Controls.Add(chkTelevisor);
             Controls.Add(lstPersonas);
             Controls.Add(lstTipo);
-            Controls.Add(chkTarjeta);
-            Controls.Add(chkEfectivo);
+            Controls.Add(optTarjeta);
+            Controls.Add(optEfectivo);
             Controls.Add(btnAceptar);
-            Controls.Add(txtTelefono);
             Controls.Add(txtNombre);
             Controls.Add(lblTelefono);
             Controls.Add(lblTitular);
@@ -316,12 +371,16 @@
             Controls.Add(panel4);
             Controls.Add(panel1);
             Controls.Add(panel2);
+            guna2Transition1.SetDecoration(this, Guna.UI2.AnimatorNS.DecorationType.None);
+            FormBorderStyle = FormBorderStyle.SizableToolWindow;
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "frmReserva";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Reserva cabañas";
             Load += frmReserva_Load;
             ((System.ComponentModel.ISupportInitialize)numDias).EndInit();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -336,11 +395,9 @@
         private Label lblTitular;
         private Label lblTelefono;
         private TextBox txtNombre;
-        private TextBox txtTelefono;
         private Button btnAceptar;
-        private System.ComponentModel.BackgroundWorker backgroundWorker1;
-        private RadioButton chkEfectivo;
-        private RadioButton chkTarjeta;
+        private RadioButton optEfectivo;
+        private RadioButton optTarjeta;
         private ComboBox lstTipo;
         private ComboBox lstPersonas;
         private NumericUpDown numDias;
@@ -352,5 +409,8 @@
         private Label lblAdicionales;
         private Panel panel1;
         private Panel panel2;
+        private Guna.UI2.WinForms.Guna2Transition guna2Transition1;
+        private Guna.UI2.WinForms.Guna2BorderlessForm guna2BorderlessForm1;
+        private MaskedTextBox txtNumero;
     }
 }
--- pryZarateSP2/frmReserva.cs
+++ pryZarateSP2/frmReserva.cs
@@ -10,21 +10,22 @@ using System.Windows.Forms;
 
 namespace pryZarateSP2
 {
-    public partial class frmReserva : Form {
+    public partial class frmReserva : Form
+    {
         const int addedValue = 1;
 
-        enum precios
-    {
-        A=20,
-        B=34
-        DailyValue= 1,
-        TvValue=2,
-        KitchenValue= 1,
+        enum Precios
+        {
+            A = 20,
+            B = 34,
+            DailyValue = 1,
+            TvValue = 2,
+            KitchenValue = 1,
+        };
+        const float fridgevalue = 1.50f;
 
-    };
 
 
-  
         public frmReserva()
         {
             InitializeComponent();
@@ -32,7 +33,18 @@ namespace pryZarateSP2
 
         private void frmReserva_Load(object sender, EventArgs e)
         {
-
+            lstTipo.Items.Clear();
+            lstTipo.Items.Add("Tipo A");
+            lstTipo.Items.Add("Tipo B");
+            lstTipo.SelectedIndex = 0;
+            numDias.Text = "1";
+            chkCocina.Checked = false;
+            chkHeladera.Checked = false;
+            chkTelevisor.Checked = false;
+            optEfectivo.Checked = true;
+            txtNombre.Text = "";
+            txtNumero.Text = "";
+            btnAceptar.Enabled = false;
         }
 
         private void txtDiastxtDias_TextChanged(object sender, EventArgs e)
@@ -44,5 +56,19 @@ namespace pryZarateSP2
         {
 
         }
+         private void txtNumero_KeyPress(object sender, KeyPressEventArgs e)
+            {
+                if (char.IsNumber(e.KeyChar) || e.KeyChar == Convert.ToChar(Keys.Back))
+                {
+                    //lblNumero.Text = "soy un numero";
+                    e.Handled = false;
+                }
+                else
+                {
+                    e.Handled = true;
+                    //lblNumero.Text = "otra cosa...";
+                }
+            }
+        }
     }
-}
+
